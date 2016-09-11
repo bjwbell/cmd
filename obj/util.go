d3965f5a61399e7933ff432f659fade255354924
JW Bell <bjwbell@gmail.com>
2016-09-11 07:27:51 -0400

    Add GOOS

--- obj/util.go
+++ obj/util.go
@@ -19,11 +19,11 @@ const (
 const (
 	// Because of masking operations in the encodings, each register
 	// space should start at 0 modulo some power of 2.
-	RBase386   = 1 * 1024
-	RBaseAMD64 = 2 * 1024
-	RBaseARM   = 3 * 1024
-	RBasePPC64 = 4 * 1024 // range [4k, 8k)
-	RBaseARM64 = 8 * 1024 // range [8k, 12k)
+	RBase386    = 1 * 1024
+	RBaseAMD64  = 2 * 1024
+	RBaseARM    = 3 * 1024
+	RBasePPC64  = 4 * 1024  // range [4k, 8k)
+	RBaseARM64  = 8 * 1024  // range [8k, 12k)
 	RBaseMIPS64 = 13 * 1024 // range [13k, 14k)
 )
 
@@ -37,3 +37,7 @@ func envOr(key, value string) string {
 func Getgoos() string {
 	return envOr("GOOS", "")
 }
+
+var (
+	GOOS = envOr("GOOS", "linux")
+)
